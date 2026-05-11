377b3b4ae0911ac1a2346579d03ac579041ee3db
Sunny Xu <bimcen@gmail.com>
2026-05-11 12:56:27 +0800

    fix: UI模块导入修复

--- ui/__init__.py
+++ ui/__init__.py
@@ -3,10 +3,17 @@
 UI模块初始化
 """
 
-from .main_window import MainWindow
-from .album_picker import AlbumPickerDialog
-from .photo_picker import PhotoPickerDialog
-from .auth_dialog import AuthDialog
+try:
+    from .main_window import MainWindow
+    from .album_picker import AlbumPickerDialog
+    from .photo_picker import PhotoPickerDialog
+    from .auth_dialog import AuthDialog
+except ImportError:
+    # 直接运行时使用绝对导入
+    from flickr_downloader.ui.main_window import MainWindow
+    from flickr_downloader.ui.album_picker import AlbumPickerDialog
+    from flickr_downloader.ui.photo_picker import PhotoPickerDialog
+    from flickr_downloader.ui.auth_dialog import AuthDialog
 
 __all__ = [
     'MainWindow',
